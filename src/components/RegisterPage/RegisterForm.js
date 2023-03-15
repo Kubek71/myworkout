47866e7c17cb6e6f0b593efd9017910c4df4ca17
Jakub Pala <pala.kuba99@gmail.com>
2023-03-16 00:24:28 +0100

    refactoring register page files, creating another component for form content, form schema, fixing styles

--- src/components/RegisterPage/RegisterForm.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { RegisterFormStyled } from "./RegisterPageStyled";
-export default function RegisterForm(props) {
-  return <RegisterForm>RegisterForm</RegisterForm>;
-}
