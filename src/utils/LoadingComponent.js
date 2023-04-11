a357f071e4ee20540c27f997e8d8126d39006387
Jakub Pala <pala.kuba99@gmail.com>
2023-04-11 18:00:17 +0200

    creating component that removes workout plan from firestore on RemoveIcon click at WorkoutProgram.js, also fixing the problem with routing (when firebase/auth is still processing to update userState) by creating loading state in authContext.js

---
+++ src/utils/LoadingComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { LoadingComponentStyled } from "../components/styles/LoadingComponentStyled";
+
+console.log("sadasd");
+
+export default function LoadingComponent() {
+  return (
+    // <LoadingComponentStyled>
+    //   <span>...</span>
+    // </LoadingComponentStyled>
+    <h1>sadsadsdadassda</h1>
+  );
+}
