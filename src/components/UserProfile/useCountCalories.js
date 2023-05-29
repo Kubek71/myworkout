3e21014ccfa1765e63f3acd0e66392e6e2d0f818
Jakub Pala <pala.kuba99@gmail.com>
2023-05-29 14:22:51 +0200

    adding another select to the register form for user's activity, updating countcalorie fn with user activity

--- src/components/UserProfile/useCountCalories.js
+++ src/components/UserProfile/useCountCalories.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 export default function useCountCalories() {
-  const countCalories = (weight, height, isMale, age) => {
+  const countCalories = (weight, height, isMale, age, activity) => {
     console.log(`${weight}, ${height} , ${age}`);
     console.log(isMale);
     const result = isMale
       ? // prettier-ignore
-        1.2 * ((10 * weight) + (6.25 * height) - (5 * age +5))
+        Math.round(activity * ((10 * weight) + (6.25 * height) - (5 * age +5)))
       : // prettier-ignore
-        1.2 * ((10 * weight) + (6.25 * height) - (5 * age - 161));
+        Math.round(activity * ((10 * weight) + (6.25 * height) - (5 * age - 161)));
 
     return result;
   };
